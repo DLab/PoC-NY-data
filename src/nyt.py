49791ad10031b7dbcc08e2e0cff8ac6d554f853f
fzmolina <molina.ita.hd@gmail.com>
2020-11-03 13:13:03 -0300

    first raw data

---
+++ src/nyt.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')
+
+print(df.columns.to_list())
+
+temp = df.loc[df['state'] == 'New York']
+
+temp['fips'].fillna(99999,inplace = True)
+
+df_ny = temp.astype({'fips': 'int32'}).copy()
+
+df_ny.sort_values('date', inplace= True)
+
+df_ny.reset_index(inplace = True, drop = True)
+
+df_ny.to_csv('../output/raw_epidemiology_NY_std.csv', index = False)
