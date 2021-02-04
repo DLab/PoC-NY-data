095f9574e464f9f7206d8f8e896eddaf7800c102
fzmolina <molina.ita.hd@gmail.com>
2021-02-04 16:01:53 -0300

    more testing

--- src/healthd.py
+++ src/healthd.py
@@ -3,8 +3,6 @@ import sys
 import numpy as np
 import utils
 import re
-import math
-import requests
 from itertools import groupby
 
 '''
@@ -50,8 +48,8 @@ class hospitalData:
         df = pd.read_csv(csv_path)
         print('file read')
 
-        cnt_data = pd.read_csv('../input/utilities/population_NY_2019_versionJH.csv')
-        hosp_list = pd.read_csv('../input/utilities/list_hospital_NY.csv')
+        self.cnt_data = pd.read_csv('../input/utilities/population_NY_2019_versionJH.csv')
+        self.hosp_list = pd.read_csv('../input/utilities/list_hospital_NY.csv')
 
         df2 = utils.hospitalData(df)
 
@@ -59,22 +57,56 @@ class hospitalData:
         ny.sort_values('fips', inplace=True)
         ny['county'] = ny['fips'].copy()
 
-        for row in cnt_data['fips'].index:
+        for row in self.cnt_data['fips'].index:
 
-            idx = ny.loc[ny['fips'] == cnt_data['fips'].iloc[row]].index
-            ny.loc[idx, 'county'] = cnt_data.loc[row, 'county']
+            idx = ny.loc[ny['fips'] == self.cnt_data['fips'].iloc[row]].index
+            ny.loc[idx, 'county'] = self.cnt_data.loc[row, 'county']
 
-        self.ny_hosp = ny.sort_values(by='date')
+        self.ny_hosp = ny.sort_values(by=['date', 'fips'])
 
-        for row in hosp_list['fips']:
+        for row in self.hosp_list['fips']:
             idx = ny.loc[ny['fips'] == row].index
 
             if len(idx) == 0:
                 print('There is a new Hospital in the state. Please complete the file ../input/utilities/list_hospital_NY.csv')
 
+        self.ny_hosp['fips'] = self.ny_hosp['fips'].astype(int)
 
+    def groupCounty(self):
 
-#    def groupCounty(self):
+        identifiers = ['date', 'fips', 'county', 'hospital_pk']
+        variables = [x for x in self.ny_hosp.columns if x not in identifiers]
+
+        self.ny_hosp[variables] = self.ny_hosp[variables].replace(-999999.0, 0.0)
+
+        listDates = self.ny_hosp['date'].unique()
+
+        hosp_county = pd.DataFrame()
+
+        i = 0
+        for row in listDates:
+            aux1 = self.ny_hosp.loc[self.ny_hosp['date'] == row]
+            for code in self.cnt_data['fips']:
+                idx = aux1[variables].loc[aux1['fips'] == code].sum(axis = 0)
+                idx2 = aux1[identifiers].loc[aux1['fips'] == code]
+                if idx2.size == 0:
+                    cnt = self.cnt_data['county'].loc[self.cnt_data['fips'] == code].item()
+                    date = self.ny_hosp['date'].loc[self.ny_hosp['date'] == row].copy()
+                    idx2[identifiers] = [date, code, cnt, 'NA']
+                    print(cnt)
+                if idx.size > 0:
+                    aux2 = pd.concat([idx2.iloc[0],idx], axis = 0)
+                else:
+                    aux2 = pd.DataFrame(np.zeros(1,len(variables)))
+                    aux2 = pd.concat([idx2, aux2], axis = 0)
+                if i == 0:
+                    hosp_county = aux2
+                    i += 1
+                else:
+                    hosp_county = pd.concat([hosp_county,aux2], axis = 0)
+
+        print(hosp_county)
+        hosp_county.to_csv('../output/Hospital-Data/test.csv', index = False)
 
 #    def groupState(self):
 
@@ -84,8 +116,7 @@ class hospitalData:
             self.ny_hosp.reset_index(inplace = True)
             self.ny_hosp.drop(columns = {'index'}, axis = 1,inplace =  True)
 
-            #self.ny_hosp.to_csv('../output/raw_hopitalData_' + label + '_NY_std.csv', float_format = '%.f', index = False)
-            self.ny_hosp.to_csv('../output/raw_hopitalData_' + label + '_NY_std.csv', index = False)
+            self.ny_hosp.to_csv('../output/Hospital-Data/raw_hopitalData_' + label + '_NY_std.csv', index = False)
 
 
         print('ALLES CLAAAARRRRRR')
@@ -97,4 +128,5 @@ if __name__ == '__main__':
         my_url = sys.argv[1]
         hosp_data = hospitalData(my_url, '../output/HealthGov/HealthGovraw_hospital_NY_std.csv')
         hosp_data.retrieveLastFileData()
+        hosp_data.groupCounty()
         hosp_data.writeData('Hospital')
